1069d50379d32283ed2faa9526688579fa7210a0
Pawan42002 <pawankumarnandagiri4901@gmail.com>
2022-06-05 02:46:25 +0530

    navbar edited

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   const location = useLocation();
   return (
     <>
-      <nav className="flex justify-between bg-white text-black py-4">
+      <nav className="hidden md:flex justify-between bg-white text-black py-4">
         <ul className="flex mx-4">
           <li className="cursor-pointer font-bold ">
             <Link to="/">RentIt</Link>
@@ -28,23 +28,26 @@ const Navbar = () => {
         <ul className="flex mx-5 font-medium space-x-2">
           <li className=" cursor-pointer hover:text-purple-500">Profile</li>
 
-          <li className="md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </li>
+          <li className="md:hidden"></li>
         </ul>
+      </nav>
+      <nav className="flex justify-end bg-white text-black py-4 px-4 space-x-96 md:hidden">
+        <div className="mx-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </div>
       </nav>
     </>
   );
