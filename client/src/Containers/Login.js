f837bcfda9a461020531b2e8a4942c904dee0fa8
Pawan42002 <pawankumarnandagiri4901@gmail.com>
2022-06-06 00:31:01 +0530

    minor changes

--- client/src/Containers/Login.js
+++ client/src/Containers/Login.js
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 function Login() {
-	const [email_id, setEmail_id] = useState("");
-	const [password, setPassword] = useState("");
-	const [isSeller, setIsSeller] = useState(false);
-	const [message, setMessage] = useState("");
-	const navigate = useNavigate();
+  const [email_id, setEmail_id] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSeller, setIsSeller] = useState(false);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-	const handleSubmit = () => {};
+  const handleSubmit = () => {};
 
-	return (
-		<div className="h-screen py-4 flex flex-col flex-grow items-center justify-center bg-gray-100 dark:bg-gray-800">
-			<div className="flex flex-col bg-white dark:bg-gray-700 justify-center shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-lg w-50 max-w-md">
-				<div className="font-medium self-center text-xl sm:text-3xl text-gray-800 dark:text-white">
-					Welcome Back
-				</div>
-				<div className="mt-4 self-center text-xl sm:text-sm text-gray-800 dark:text-white">
-					Enter your credentials to access your account
-				</div>
+  return (
+    <div className="h-screen py-4 flex flex-col flex-grow items-center justify-center bg-gray-100 dark:bg-gray-800">
+      <div className="flex flex-col bg-white dark:bg-gray-700 justify-center shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-lg w-50 max-w-md">
+        <div className="font-medium self-center text-xl sm:text-3xl text-gray-800 dark:text-white">
+          Welcome Back
+        </div>
+        <div className="mt-4 self-center text-xl sm:text-sm text-gray-800 dark:text-white">
+          Enter your credentials to access your account
+        </div>
 
-				<div className="mt-10">
-					<div className="flex flex-col mb-5">
-						<label className="mb-1 text-xs tracking-wide text-gray-600 dark:text-gray-400">
-							E-Mail Address:
-						</label>
-						<div className="relative">
-							<div
-								className="
+        <div className="mt-10">
+          <div className="flex flex-col mb-5">
+            <label className="mb-1 text-xs tracking-wide text-gray-600 dark:text-gray-400">
+              E-Mail Address:
+            </label>
+            <div className="relative">
+              <div
+                className="
                                   inline-flex
                                   items-center
                                   justify-center
@@ -36,30 +36,30 @@ function Login() {
                                   h-full
                                   w-10
                                   text-gray-400"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									strokeWidth="2"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-									/>
-								</svg>
-							</div>
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+              </div>
 
-							<input
-								id="email"
-								type="email"
-								name="email"
-								value={email_id}
-								onChange={(e) => setEmail_id(e.target.value)}
-								className="
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={email_id}
+                onChange={(e) => setEmail_id(e.target.value)}
+                className="
                                     dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700
                                       dark:border-gray-800 
                                       text-sm
@@ -71,17 +71,17 @@ function Login() {
                                       w-full
                                       py-2
                                       focus:outline-none focus:border-blue-400"
-								placeholder="Enter your email"
-							/>
-						</div>
-					</div>
-					<div className="flex flex-col mb-6">
-						<label className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600 dark:text-gray-400">
-							Password:
-						</label>
-						<div className="relative">
-							<div
-								className="
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col mb-6">
+            <label className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600 dark:text-gray-400">
+              Password:
+            </label>
+            <div className="relative">
+              <div
+                className="
                                       inline-flex
                                       items-center
                                       justify-center
@@ -91,30 +91,30 @@ function Login() {
                                       h-full
                                       w-10
                                       text-gray-400"
-							>
-								<span>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										className="h-5 w-5"
-										viewBox="0 0 20 20"
-										fill="currentColor"
-									>
-										<path
-											fillRule="evenodd"
-											d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-											clipRule="evenodd"
-										/>
-									</svg>
-								</span>
-							</div>
+              >
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </div>
 
-							<input
-								id="password"
-								type="password"
-								name="password"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								className="
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="
                                     dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700
                                       dark:border-gray-800 
                                       text-sm
@@ -126,32 +126,32 @@ function Login() {
                                       w-full
                                       py-2
                                       focus:outline-none focus:border-blue-400"
-								placeholder="Enter your password"
-							/>
-						</div>
-					</div>
+                placeholder="Enter your password"
+              />
+            </div>
+          </div>
 
-					<div className="flex items-center mb-4">
-						<input
-							id="checkbox-3"
-							aria-describedby="checkbox-3"
-							type="checkbox"
-							className="bg-gray-50 dark:bg-gray-800 dark:text-purple-700 dark:hover:bg-purple-800 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
-							checked={isSeller}
-							onChange={() => setIsSeller(!isSeller)}
-						/>
-						<label
-							for="checkbox-3"
-							className="text-sm ml-3 font-medium text-gray-900 dark:text-gray-100"
-						>
-							Are you a seller?
-						</label>
-					</div>
+          <div className="flex items-center mb-4">
+            <input
+              id="checkbox-3"
+              aria-describedby="checkbox-3"
+              type="checkbox"
+              className="bg-gray-50 dark:bg-gray-800 dark:text-purple-700 dark:hover:bg-purple-800 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
+              checked={isSeller}
+              onChange={() => setIsSeller(!isSeller)}
+            />
+            <label
+              for="checkbox-3"
+              className="text-sm ml-3 font-medium text-gray-900 dark:text-gray-100"
+            >
+              Are you a seller?
+            </label>
+          </div>
 
-					<div className="flex w-full flex-col">
-						<button
-							onClick={handleSubmit}
-							className="
+          <div className="flex w-full flex-col">
+            <button
+              onClick={handleSubmit}
+              className="
                                       flex
                                       mt-2
                                       items-center
@@ -164,48 +164,48 @@ function Login() {
                                       transition
                                       duration-150
                                       ease-in"
-						>
-							<span className="mr-2 uppercase">Sign In</span>
-							<span>
-								<svg
-									className="h-6 w-6"
-									fill="none"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-								</svg>
-							</span>
-						</button>
-						<div className="text-xs p-2 text-red-600">{message}</div>
-					</div>
-				</div>
-			</div>
-			<div className="flex justify-center items-center mt-6">
-				<div
-					className="
+            >
+              <span className="mr-2 uppercase">Sign In</span>
+              <span>
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </span>
+            </button>
+            <div className="text-xs p-2 text-red-600">{message}</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-6">
+        <div
+          className="
                       inline-flex
                       items-center
                       text-gray-700
                       font-medium
                       text-xs text-center"
-				>
-					<span className="ml-2">
-						You don't have an account?
-						<Link
-							to="/register"
-							className="text-xs ml-2 text-indigo-600 hover:text-indigo-500 font-semibold"
-						>
-							Register now
-						</Link>
-					</span>
-				</div>
-			</div>
-		</div>
-	);
+        >
+          <span className="ml-2">
+            You don't have an account?
+            <Link
+              to="/register"
+              className="text-xs ml-2 text-indigo-600 hover:text-indigo-500 font-semibold"
+            >
+              Register now
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
