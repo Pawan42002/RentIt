7b43b415e217b38c9b8fbedf80a3c7f67b674a16
Pawan42002 <pawankumarnandagiri4901@gmail.com>
2022-07-05 19:05:52 +0530

    logout added in backend

--- server/index.js
+++ server/index.js
@@ -10,16 +10,16 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-	);
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 connectToMongo();
@@ -29,14 +29,19 @@ app.use("/api/listings", require("./routes/listings"));
 
 app.use("/api/businessAuth", require("./routes/businessAuth"));
 app.get("/", (req, res) => {
-	res.send("home");
+  res.send("home");
 });
 
 app.get("/getUserData", fetchUser, async (req, res) => {
-	console.log("here");
-	res.send({ isLandlord: req.user.isLandlord });
+  console.log("here");
+  res.send({ isLandlord: req.user.isLandlord });
+});
+
+app.post("/logout", async (req, res) => {
+  res.clearCookie();
+  res.send("Logout successful");
 });
 
 app.listen(3005, () => {
-	console.log("Running on port 3005");
+  console.log("Running on port 3005");
 });
