2f7de4bac9c05cfb54947108885a08ecf17863d4
Pawan42002 <pawankumarnandagiri4901@gmail.com>
2022-07-05 19:21:02 +0530

    fetchUser changed

--- server/middleware/fetchUser.js
+++ server/middleware/fetchUser.js
@@ -2,21 +2,19 @@ var jwt = require("jsonwebtoken");
 const JWT_SECRET = "pawan";
 
 const fetchUser = async (req, res, next) => {
-	// Get the user from the jwt token and add id to req object
-	const token = req.cookies.access_token;
-	console.log("her@1");
-	if (!token) {
-		console.log("her@");
-		//res.status(401).send({ error: "Please authenticate using a valid token" });
-		return res.sendStatus(403);
-	}
-	try {
-		const data = jwt.verify(token, JWT_SECRET);
-		req.user = data.user;
-		return next();
-	} catch (error) {
-		return res.sendStatus(403);
-	}
+  // Get the user from the jwt token and add id to req object
+  const token = req.cookies.access_token;
+  if (!token) {
+    //res.status(401).send({ error: "Please authenticate using a valid token" });
+    return res.send("no token found");
+  }
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = data.user;
+    return next();
+  } catch (error) {
+    return res.sendStatus(403);
+  }
 };
 
 module.exports = fetchUser;
