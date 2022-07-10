a8d3b5e02e0821f4f549825f21640c27f8ca1edb
Manjunath0408 <nath.vasam@gmail.com>
2022-07-10 17:58:14 +0530

    modal functional

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RentIt
+
 # RentIt
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAThitiSwcP-APwFJMsYlng5FYbaZTMvW8",
-  authDomain: "rent-it-f326a.firebaseapp.com",
-  projectId: "rent-it-f326a",
-  storageBucket: "rent-it-f326a.appspot.com",
-  messagingSenderId: "83118044625",
-  appId: "1:83118044625:web:3ac1c7849462e02d55dcf0",
-  measurementId: "G-2G02N671TB",
+	apiKey: "AIzaSyAThitiSwcP-APwFJMsYlng5FYbaZTMvW8",
+	authDomain: "rent-it-f326a.firebaseapp.com",
+	projectId: "rent-it-f326a",
+	storageBucket: "rent-it-f326a.appspot.com",
+	messagingSenderId: "83118044625",
+	appId: "1:83118044625:web:3ac1c7849462e02d55dcf0",
+	measurementId: "G-2G02N671TB",
 };
 
 // Initialize Firebase
