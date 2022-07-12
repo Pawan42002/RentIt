40ac367062511979ecbe8ae961d2b3e2d3911f55
Pawan42002 <pawankumarnandagiri4901@gmail.com>
2022-07-12 15:48:04 +0530

    error fixed

--- client/src/Containers/Business/MyListings.js
+++ client/src/Containers/Business/MyListings.js
@@ -32,7 +32,7 @@ const MyListings = () => {
         <>
           <div className="grid grid-cols-1 md:grid-cols-2 mx-auto justify-items-center gap-x-4 gap-y-4 max-w-fit mt-5">
             {listings.map((listing, idx) => {
-              return <ListSummary key={idx} />;
+              return <ListSummary listing={listing} key={idx} />;
             })}
           </div>
         </>
