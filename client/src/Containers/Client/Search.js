d9352f401e104788ce72c09a8f0bf0e33a4e5f45
Pawan42002 <pawankumarnandagiri4901@gmail.com>
2022-07-20 15:24:10 +0530

    search page cleaned

--- client/src/Containers/Client/Search.js
+++ client/src/Containers/Client/Search.js
@@ -4,10 +4,10 @@ import ListSummary from "../../Components/ListSummary";
 function Search() {
   return (
     <div className="dark:bg-gray-800 dark:text-white bg-gray-100 ">
-      <div className="max-w-3xl mx-auto p-5 ">   
+      <div className="max-w-3xl mx-auto p-5 ">
         <button
           type="text"
-          className="flex mx-auto items-center w-full text-left space-x-3 px-4 h-12 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-purple-700 shadow-sm rounded-lg text-slate-400 dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700 focus:ring-purple-700"        
+          className="flex mx-auto items-center w-full text-left space-x-3 px-4 h-12 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-purple-700 shadow-sm rounded-lg text-slate-400 dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700 focus:ring-purple-700"
         >
           <svg
             width="24"
@@ -29,16 +29,10 @@ function Search() {
             placeholder="Enter a location.."
           ></input>
         </button>
-        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto justify-items-center gap-x-4 gap-y-4 max-w-fit mt-5">
-            <ListSummary />
-            <ListSummary />
-            <ListSummary />
-            <ListSummary />
-        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto justify-items-center gap-x-4 gap-y-4 max-w-fit mt-5"></div>
       </div>
     </div>
   );
 }
 
 export default Search;
-
