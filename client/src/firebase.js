760083934f29b3009fc5786fb4be0eca275898e2
Pawan42002 <91836724+Pawan42002@users.noreply.github.com>
2024-08-10 13:31:02 +0530

    fix firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,17 +8,15 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyDpdZkXnrAJ0HEqqqagBWp3THqgcC5VnKI",
-	authDomain: "rent-it3.firebaseapp.com",
-	projectId: "rent-it3",
-	storageBucket: "rent-it3.appspot.com",
-	messagingSenderId: "577773763832",
-	appId: "1:577773763832:web:bf0b41a267548bb748cf0d",
-	measurementId: "G-DLCFHXPL0R",
+	apiKey: "AIzaSyC3vp3vBoXYygQGDwol9tyJRJJHs4RAS90",
+	authDomain: "rentithost.firebaseapp.com",
+	projectId: "rentithost",
+	storageBucket: "rentithost.appspot.com",
+	messagingSenderId: "903531925599",
+	appId: "1:903531925599:web:3e4ed7c446f5a3cbd80bea",
+	measurementId: "G-C99Q3SZ2W3",
 };
-
 // Initialize Firebase
-
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const storage = getStorage(app);
